921cf5708a17d9886b4acbd6b0ec3ec23a867a64
Iker-hub <ikergargu@gmail.com>
2023-01-30 03:07:07 +0100

    Upload files

--- game.js
+++ game.js
@@ -19,20 +19,74 @@ export class Game extends Phaser.Scene {
    * Cuando está todo precargado se invoca create()
    */
   preload() {
-    // Precargamos dos imágenes
+    // Precargamos las imágenes
     // Params:          id asset,   ruta
     this.load.image("background", "assets/images/background.png");
     this.load.image("gameover", "assets/images/gameover.png");
+    this.load.image("platform", "assets/images/platform.png");
+    this.load.image("ball", "assets/images/ball.png");
   }
 
   /**
    * Colocar todos los elementos de la escena
    */
   create() {
+    // Establecemos colisiones en los bordes del mapa, excepto en el inferior
+    this.physics.world.setBoundsCollision(true, true, true, false);
+
     // Añadimos una imagen al juego
     // Las coordenadas parten de arriba a la izquierda y las imgs se colocan
     // según su centro
     //Params:     coorX, coorY, id imagen
     this.add.image(400, 250, "background");
+    this.gameoverImage = this.add.image(400, 90, "gameover"); // Guardamos la imagen en un obj
+    this.gameoverImage.visible = false; // Ocultamos el gameover
+
+    // Sistema de físicas, lo usamos cuando colocamos un asset que le afecta las físicas
+    this.platform = this.physics.add.image(400, 460, "platform").setImmovable(); // Hacemos la plataforma inmovible
+    // Platform no es afectado por la gravedad
+    this.platform.body.allowGravity = false;
+    this.platform.setCollideWorldBounds(true);
+
+    this.ball = this.physics.add.image(400, 30, "ball");
+
+    // Generemos velocidad random multiplicando 100 * nº entre 1.3 y 2
+    let velocity = 100 * Phaser.Math.Between(1.3, 2);
+    if (Phaser.Math.Between(0, 10) > 5) {
+      // Aleatoriamente la velocidad será left o right, cambiando el signo
+      velocity = 0 - velocity;
+    }
+    // Seteamos la velocidad random en la coorX, además de una en la coorY
+    this.ball.setVelocity(velocity, 10);
+    // La bola rebotará en los bordes con colisiones
+    this.ball.setCollideWorldBounds(true);
+
+    // Añadimos colisión entre la bola y la plataforma
+    this.physics.add.collider(this.ball, this.platform);
+
+    // Cuando la bola impacte rebotará con la misma fuerza, ya que el param es 1
+    this.ball.setBounce(1);
+
+    // Accedemos a las teclas
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  /**
+   * Método que actualiza constantemente el juego
+   */
+  update() {
+    if (this.cursors.left.isDown) {
+      this.platform.setVelocityX(-500); // Velocidad al platform en la coorX
+    } else if (this.cursors.right.isDown) {
+      this.platform.setVelocityX(500);
+    } else {
+      this.platform.setVelocityX(0);
+    }
+
+    // Si la bola sale del mapa, gameover y pausamos la escena
+    if (this.ball.y > 500) {
+      this.gameoverImage.visible = true;
+      this.scene.pause();
+    }
   }
 }
